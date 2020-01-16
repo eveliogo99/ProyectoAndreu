a3b6108a86b1463c8f19d14c492b40adee58fd84
kosmoMBL <57523273+kosmoMBL@users.noreply.github.com>
2020-01-16 20:28:26 +0100

    Add files via upload

--- content.php
+++ content.php
@@ -1,41 +1,70 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form action="">
-	<p>Username:</p>
-	<input type="text" name="login"></input>
+	Quien eres?<br>
+	<input type="radio" name="tipoLogin" onclick="mostraUsu()"> Usuario<br>
+  <input type="radio" name="tipoLogin"onclick="mostraEmpresa()"> Empresa<br>
+
+	<div id="usuario" style="display: none">
+	<form action="">
+		Login usuario:
+	<p>Nom de l´usuari:</p>
+	<input type="text" name="loginU"></input>
 	<p>Password:</p>
-	<input type="password" name="password"></input>
+	<input type="password" name="passwordU"></input>
+<br/>
+	<input type="submit" value="Login"></input>
+</form>
+</div>
+<div id="empresa" style="display: none">
+<form action="">
+	Login empresa:
+	<p>Nom de l´empresa:</p>
+	<input type="text" name="loginE"></input>
+	<p>Password:</p>
+	<input type="password" name="passwordE"></input>
 <br/>
 	<input type="submit" value="Login"></input>
 
 </form>
-
+</div>
 <p>El resultat:</p>
-
+<script>
+	usuario=document.getElementById("usuario");
+	empresa=document.getElementById("empresa");
+	function mostraUsu() {	
+		usuario.style.display='block';
+		empresa.style.display='none';
+	}
+	function mostraEmpresa() {
+		usuario.style.display='none';
+		empresa.style.display='block';
+	}
+</script>
 <?php
-$user = $_GET['login'];
-$pass = $_GET['password'];
-
-
-
+$userU = $_GET['loginU'];
+$passU = $_GET['passwordU'];
+$userE = $_GET['loginE'];
+$passE = $_GET['passwordE'];
 $enlace = mysqli_connect("127.0.0.1:3306", "jordi", "patata", "phplogin");
-
 if (!$enlace) {
     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
     echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
     echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-$result = mysqli_query($enlace,"SELECT * FROM `users` WHERE Name='$user' and Password='$pass'");
-if(mysqli_num_rows($result)==0) echo("user was not found"); 
-
+//Login para usuario
+$resultU = mysqli_query($enlace,"SELECT * FROM 'usuarios' WHERE Name='$userU' and Password='$passU'");
+if(mysqli_num_rows($resultU)==0) echo("No s´ha trobat l´usuari"); 
 else { echo("user was found"); 
 	   
 	 }
-
-
-
+//Login para empresa
+$resultE = mysqli_query($enlace,"SELECT * FROM 'empresa' WHERE Name='$userE' and Password='$passE'");
+if(mysqli_num_rows($resultE)==0) echo("No s´ha trobat el nom de la empresa"); 
+else { echo("user was found"); 
+	   
+	 }
 mysqli_close($enlace);
 ?>
 

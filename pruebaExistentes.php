4c3469524a3b2c7a4b2171a349ee28063f979c8c
Evelio Gonzalez Ortega <eveliogonzalez099@gmail.com>
2020-01-22 17:51:49 +0100

    chekear usuario

---
+++ pruebaExistentes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Existentes</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <div class="form-group">
+            <label for="username">Usuario</label>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Introduce tu nombre...">
+            <small class="form-text text-muted">Elige "jose" o "maria" siendo usuarios que ya existen en base de datos.</small><br/>
+            <div id="result-username"></div>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Escribe tu password aqui...">
+        </div>  
+    </form>
+</body>
+</html>
